06e27ca07720e15f23ba1856e7bfdd83d6b4428f
justin0979 <justin0979@gmail.com>
2019-08-04 15:47:11 -0500

    added css to header text

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,10 +6,10 @@ import './Header.scss';
 const Header = () => {
   return (
     <div className="menu">
-      <Link to="/elijahs-retreat-experience/activity">Activity</Link>
-      <Link to="/elijahs-retreat-experience">Home</Link>
-      <Link to="/elijahs-retreat-experience/animals">Animals</Link>
-      <Link to="/elijahs-retreat-experience/animalfeed">Animal Feedings</Link>
+      <Link className='title' to="/elijahs-retreat-experience/activity">Activity</Link>
+      <Link className='title' to="/elijahs-retreat-experience">Home</Link>
+      <Link className='title' to="/elijahs-retreat-experience/animals">Animals</Link>
+      <Link className='title' to="/elijahs-retreat-experience/animalfeed">Animal Feedings</Link>
     </div>
   );
 };
