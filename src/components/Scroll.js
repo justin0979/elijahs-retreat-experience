f357f5974c0c7dc621ae1b5bc906c3162c8adb68
justin <justin0979@gmail.com>
2019-09-16 21:41:29 -0500

    adjusted menu for phone size

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -1,11 +1,7 @@
 import React from 'react';
 
 const Scroll = props => {
-  return (
-    <div style={{overflowY: 'scroll', height: '90vh'}}>
-      {props.children}
-    </div>
-  )
-}
+  return <div className="scroll">{props.children}</div>;
+};
 
 export default Scroll;
